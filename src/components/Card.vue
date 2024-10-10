22f4f35c3ec7634a558e6ea3cc07bb55b7a28f26
João Victor <jcarv.contato@gmail.com>
2024-10-10 08:03:39 -0300

    First commit

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const cardsArray: Array<string> = ref([
+  '🐶', '🐶', '🐱', '🐱', '🦊', '🦊', '🐻', '🐻','🐵','🐵','🦝','🦝',
+  '🐼', '🐼', '🐨', '🐨', '🐯', '🐯', '🦁', '🦁','🐭','🐭','🐷','🐷'
+]);
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
